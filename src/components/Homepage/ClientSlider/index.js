8c2eb73ba300917a1b479389681c14f9a71e118e
Mahabub Hossain <sjl.hsn@gmail.com>
2021-09-09 02:19:38 +0600

    Update index.js

--- src/components/Homepage/ClientSlider/index.js
+++ src/components/Homepage/ClientSlider/index.js
@@ -32,41 +32,41 @@ const ClientSlider = () => {
                 itemClass="image-item"
                 responsive={responsive}
             >
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/pizza-hunt.jpg" alt="pizza-hut" />
+                <div>
+                    <StaticImage src="../../../images/clients/pizza-hunt.jpg" alt="pizza-hut" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/tacobell.jpg" alt="tacobell" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/pf-change.jpg" alt="pf-change" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/paho.jpg" alt="paho" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/pandora.jpg" alt="Pandora" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/departmento.jpg" alt="departmento" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/cba.jpg" alt="cba" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/boolchands.jpg" alt="boolchands" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/apex.jpg" alt="apex" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/cbn.jpg" alt="cbn" />
+                </div>
+                <div>
+                    <StaticImage src="../../../images/clients/cmb.jpg" alt="cmb" />
                 </div>
                 <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/tacobell.jpg" alt="tacobell" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/pf-change.jpg" alt="pf-change" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/paho.jpg" alt="paho" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/pandora.jpg" alt="Pandora" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/departmento.jpg" alt="departmento" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/cba.jpg" alt="cba" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/boolchands.jpg" alt="boolchands" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/apex.jpg" alt="apex" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/cbn.jpg" alt="cbn" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/cmb.jpg" alt="cmb" />
-                </div>
-                <div >
-                    <StaticImage className={styles.image} src="../../../images/clients/gda.jpg" alt="gda" />
+                    <StaticImage src="../../../images/clients/gda.jpg" alt="gda" />
                 </div>
             </Carousel>
             </div>
