c67279554bef91b3b895e078961cde3b22ed4d1b
Mahabub Hossain <sjl.hsn@gmail.com>
2021-09-10 00:52:39 +0600

    style fix.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,14 @@
-import { Link } from "gatsby"
 import * as React from "react"
 import Layouts from "../components/Layouts"
 
 // markup
 const NotFoundPage = () => {
   return (
-    <Layouts path={"/not"} title={"404 not Found"} description={""}>
-      <h1>Page not found</h1>
-      <p>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <Layouts path={"/404"} title={"404 not Found"} description={""}>
+      <div style={{width: '84%', margin: '60px auto'}}>
+        <h1 style={{fontFamily: 'Barlow', marginBottom: '20px'}}>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
     </Layouts>
   )
 }
